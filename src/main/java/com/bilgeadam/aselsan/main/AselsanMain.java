a23c36744bdf8f179cbfdd83f750f27f01570e4c
hamitmizrak <hamitmizrak@gmail.com>
2021-12-06 10:44:00 +0300

    first commit

---
+++ src/main/java/com/bilgeadam/aselsan/main/AselsanMain.java
@@ -0,0 +1,8 @@
+package com.bilgeadam.aselsan.main;
+
+public class AselsanMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Merahabalar Aselsana Eğitime hoşgeldiniz");
+	}
+}
